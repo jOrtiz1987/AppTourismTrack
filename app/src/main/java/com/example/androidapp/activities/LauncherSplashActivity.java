c10c53b13033e88dc78a90fe3318f99aeaa295e4
JAOrtiz19 <ic.jortiz@gmail.com>
2026-03-04 13:01:33 -0600

    Inicio de aplicacion en login

--- app/src/main/java/com/example/androidapp/activities/LauncherSplashActivity.java
+++ app/src/main/java/com/example/androidapp/activities/LauncherSplashActivity.java
@@ -16,15 +16,9 @@ public class LauncherSplashActivity extends AppCompatActivity {
         SharedPreferences prefs = getSharedPreferences("UserPrefs", MODE_PRIVATE);
         boolean hasCompletedOnboarding = prefs.getBoolean("hasCompletedOnboarding", false);
 
-        Intent targetIntent;
-
-        if (!hasCompletedOnboarding) {
-            // Es la primera vez (o no ha terminado el onboarding), va al Registro
-            targetIntent = new Intent(this, RegisterActivity.class);
-        } else {
-            // Ya completó el ciclo, va al Login
-            targetIntent = new Intent(this, LoginActivity.class);
-        }
+        // Sin importar si completó el onboarding, vamos al Login como default a
+        // petición
+        Intent targetIntent = new Intent(this, LoginActivity.class);
 
         startActivity(targetIntent);
         finish(); // Cierra esta actividad para no volver atrás
